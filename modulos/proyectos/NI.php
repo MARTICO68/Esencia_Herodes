e9dacb7f34260d7b77c530075425dd18be6ad7eb
MARTICO68 <MARQUITOSROJAS71@GMAIL.COM>
2023-10-19 14:50:45 -0600

    yes

---
+++ modulos/proyectos/NI.php
@@ -0,0 +1,99 @@
+<?php
+include("../../conn/conn.php");
+revisarPrivilegio(4);
+$idProyecto = isset($_GET['id_proyecto']) ? $_GET['id_proyecto'] : '';
+
+
+if(isset($_POST['guardar'])){
+    $idPro = $_POST['idProyecto']; // Corrección: Obtener idProyecto desde el formulario
+    $fecha  = $_POST['fecha'];
+    $numero = $_POST['numero'];
+    $precio = $_POST['precio'];
+
+    $sqlClientes = "INSERT INTO tingresos (idProyecto, fecha, numero, precio, estado) VALUES
+    ('$idProyecto', '$fecha', '$numero', '$precio', 1)";
+    $queryClientes = mysqli_query($conn, $sqlClientes);
+    
+    ?>
+    <script> 
+        alert('Datos guardados correctamente');          
+        document.location.href = "ingresos.php?id_proyecto=<?=urlencode($idProyecto)?>"; // Corrección: Redireccionar correctamente
+    </script>
+    <?php
+    exit();
+}
+include("../../template/top.php");
+?>
+<link rel="stylesheet" href="modals.css">
+<div class="card">
+    <div class="card-body">
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <h5 class="text-center">Nuevos ingresos</h5>
+                <hr>                                              
+                <form class="user" action="" method="post">                               
+                
+                <input type="hidden" name="idProyecto" value="<?= isset($_GET['id_proyecto']) ? $_GET['id_proyecto'] : '' ?>"> <!-- Campo oculto para idProyecto -->
+                 
+                <div class="col-12 text-right">                                                                 
+                <a href="administracion.php?id_proyecto=<?=urlencode($idProyecto)?>" class="btn btn-outline-dark"><i class="fas fa-fw fa-arrow-left"></i> Regresar</a>                                  
+                </div> 
+
+                <div class="row">
+                <div class="col-md-6">                                                
+                <label for="fecha">Fecha</label>
+                <input type="date" id="fecha" name="fecha" required type="text" value="<?= $fecha?>" class="form-control" autocomplete="off">                   
+                </div>
+                </div>
+
+           
+                <div class="row">
+                    <div class="col-md-6">
+                        <label for="cajuelas"># de fanegas</label>
+                        <input type="number" id="cajuelas" oninput="calcular()" step="0.001" name="numero" required type="number" class="form-control" autocomplete="off">       
+                    </div>
+                    <br>
+                    <body>                                                                    
+                    <div class="col-md-6">
+                        <label for="precio">Precio por fanega</label>
+                        <input type="number" id="precio" oninput="calcular()" name="precio" required type="number" class="form-control" autocomplete="off">       
+                    </div>
+                    </div>  
+                                                                
+                    <div class="row">
+                    <div class="col-md-6">
+                        <label for="total">Total</label>
+                        <input type="number" id="total" step="0.001" name="total" required type="number" class="form-control"  autocomplete="off">       
+                    </div>   
+                    </div>
+        
+                    <script type="text/javascript">
+
+                        function calcular(){
+                    try{ 
+                        var a = parseFloat(document.getElementById("cajuelas").value) || 0,
+                            b = parseFloat(document.getElementById("precio").value) || 0;
+                            document.getElementById("total").value = a * b;
+                    } catch (e) {}
+
+                }        
+                </script>                                                               
+                </script>
+
+                <hr> 
+                <div class="row">                     
+                    <div class="col-12 mb-2">
+                        <button type="submit" class="btn btn-dark btn-user btn-block" name="guardar"><i class="fa fa-save"></i> Guardar</button>                               
+                </div>                                                                                                     
+            </div>                      
+        </div>
+        <div class="col-12 col-md-6 text-center">                                             
+                <img class="img-fluid" src="<?=$baseURL?>img/proyecto.gif" height="550px"><br>                                       
+            </div>                                                                                                                                                                           
+        </form>
+        
+    </div>                                                
+</div>
+<?php
+include("../../template/bottom.php");
+?>
---
+++ modulos/proyectos/ingresos.php
@@ -0,0 +1,115 @@
+<?php
+include("../../conn/conn.php");
+include("../../template/top.php");
+revisarPrivilegio(4);
+
+// Variable para almacenar el valor de búsqueda
+$buscar = '';
+
+if (isset($_GET['buscar'])) {
+    $buscar = $_GET['buscar'];
+}
+
+// Consulta SQL para buscar registros
+$sql = "SELECT * FROM tingresos WHERE estado = 1";
+
+if (!empty($buscar)) {
+    // Modifica la consulta para buscar por nombre
+    $sql .= " AND idProyecto LIKE '%$buscar%'";
+}
+
+$query = mysqli_query($conn, $sql);
+$total1 = 0; // Inicializa la variable $total
+?>
+
+<div class="card">
+    <div class="card-body">
+        <div class="row text-center">
+            <div class="col-12">
+                <h4>Ingresos</h4>
+                <hr>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 text-right p-2 mb-2">
+                <a href="administracion.php" class="btn btn-dark"><i class="fas fa-fw fa-arrow-left"></i> Regresar </a>
+            </div>
+        </div>
+
+        <form action="" method="get">
+            <div class="row text-left">
+                <div class="col-3">
+                    <input type="text" name="buscar" placeholder="Buscar por nombre" class="form-control" autocomplete="off" value="<?=$buscar?>">
+                </div>
+                <div class="col-9">
+                    <button type="submit" class="btn btn-dark">Buscar</button>
+                </div>
+            </div>
+        </form>
+
+        <?php
+        if (mysqli_num_rows($query) == 0){
+            ?>
+            <div class="row text-center">
+                <div class="col-12">
+                    <a href="NI.php" class="btn-sm btn btn-outline-success"><i class="fas fa-fw fa-plus"></i></a> 
+                    <br>              
+                    <img class="img-fluid" src="<?=$baseURL?>img/nohay.gif" ><br>         
+                    No hay registros de ingresos.  
+                </div>
+            </div>
+            <?php
+        } else {
+            ?>
+            <div class="table-responsive">
+                <table class="table table-striped table-bordered" width="100%" cellspacing="0">
+                    <thead>
+                        <th style="min-width:150px">
+                            <a href="NI.php" class="btn-sm btn btn-outline-success"><i class="fas fa-fw fa-plus"></i></a>
+                            <a href="reportesPla.php?<?=urlencode('id_proyecto=' .$_GET['id_proyecto'])?>" class="btn-sm btn btn-outline-danger"><i class="fas fa-fw fa-download"></i></a>
+                        </th>
+                        <th>Fecha</th>
+                        <th># fanegas</th>
+                        <th>Precio</th>
+                        <th>Total</th>
+                        <th>Subtotal</th>
+                    </thead>
+                    <tbody>
+                        <?php while($rowClientes=mysqli_fetch_assoc($query)){ ?>
+                        <tr>
+                            <td style="min-width:150px">
+                                <a href="EP.php?id_editar=<?=$rowClientes['id']?>" class="btn-sm btn btn-outline-dark"><i class="fa fa-fw fa-edit"></i></a>
+                                <a href="ingresos.php?id_eliminar=<?=$rowClientes['id']?>" onclick="return confirm('¿Está seguro(a) que desea eliminar?')" class="btn-sm btn btn-danger"><i class="fas fa-fw fa-trash"></i></a>
+                            </td>
+                            <td><?=$rowClientes['fecha']?></td>
+                            <td><?=$rowClientes['numero']?></td>
+                            <td><?=$rowClientes['precio']?></td>
+                            <td><?=$rowClientes['total']?></td>
+                            <td class="text-right">
+                                <?php
+                                echo '&cent; '.number_format($subtotal = $rowClientes['total'],2);
+                                $total += $subtotal;
+                                ?>
+                            </td>
+                        </tr>
+                        <?php } ?>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td class="text-right"><strong>Total</strong></td>
+                            <td class="text-right"><strong>&cent; <?=number_format($total,2)?></strong> </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        <?php
+        }
+        ?>
+    </div>
+</div>
+
+<?php
+include("../../template/bottom.php");
+?>
